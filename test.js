9c9c62bb6dcbb371295a688f4ffe9e8cf5e8ee93
vincent <vincentxman@hotmail.com>
2020-06-12 07:53:17 +0800

    console.i

--- test.js
+++ test.js
@@ -11,14 +11,18 @@ v.tools.fun(); // 已经载入 tools 了，直接调用。
 // console-color
 console.log('-'.repeat(10), 'console-color'.underline.magenta.bold);
 console.info('this is a test');
+console.i('this is a test');
 console.error('this is a test');
+console.e('this is a test');
 console.warn('this is a test');
-console.debug('this is a test\n');
+console.w('this is a test');
+console.debug('this is a test');
+console.d('this is a test\n');
 
-console.log('there is a ' + 'info'.info);
-console.log('there is a ' + 'error'.error);
-console.log('there is a ' + 'warn'.warn);
-console.log('there is a ' + 'debug\n'.debug);
+console.log('there is a ' + 'info'.info + ' too'.e);
+console.log('there is a ' + 'error'.error + ' too'.i);
+console.log('there is a ' + 'warn'.warn + ' too'.d);
+console.log('there is a ' + 'debug'.debug + ' too\n'.w);
 
 console.log('there is a '.yellow.greyBG + 'warn\n'.black.yellowBG.bold);
 
